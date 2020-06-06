1594fa2004e5cdada69c19ca8d3fda574c61a43d
Mazedur Rahman Riad <52395387+riadrahman013@users.noreply.github.com>
2020-06-06 15:14:13 +0600

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+
+    <div class="content">
+        <img src="/img/pizza-house.png" alt="pizza_house_logo">
+        <div class="title m-b-md">
+            Pizza House
+        </div>
+        <p class="msg">{{ session('msg') }}</p>
+        <a href="{{ route('pizzas.create') }}">Order a Pizza</a>
+    </div>
+</div>
+@endsection
